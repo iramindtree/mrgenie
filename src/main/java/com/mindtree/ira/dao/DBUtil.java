a1b9a3b14a2ac1d4ff3d0223cb54244520c0408d
vlaks292 <vijendra.lakshamanarao@marriott-sp.com>
2017-02-17 23:17:03 +0530

    DB connection Testing

--- src/main/java/com/mindtree/ira/dao/DBUtil.java
+++ src/main/java/com/mindtree/ira/dao/DBUtil.java
@@ -6,6 +6,8 @@ package com.mindtree.ira.dao;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @author M1031960
@@ -15,25 +17,33 @@ public class DBUtil {
 	
 	public static Connection getDBConnection(){
 		
+		Connection conn = null;
 		URI dbUri;
 		try {
-			dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+				dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 			
-			 String username = dbUri.getUserInfo().split(":")[0];
+			 	String username = dbUri.getUserInfo().split(":")[0];
 		        String password = dbUri.getUserInfo().split(":")[1];
 		        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
-		        System.out.println("Username:"+ username);
-		        System.out.println("password:"+ password);
-		        System.out.println("dbUrl:"+ dbUrl);
+		       
+		        Class.forName("com.mysql.jdbc.Driver");
+		        conn = DriverManager.getConnection(dbUrl, username, password);
+		        
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		catch(ClassNotFoundException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch(SQLException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
        
 		
-		return null;
+		return conn;
 	}
 
 }
