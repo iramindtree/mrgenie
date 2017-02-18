c64082a8c2a0db757e77ed87eae524ac06df73c7
vlaks292 <vijendra.lakshamanarao@marriott-sp.com>
2017-02-18 18:51:07 +0530

    pool cross selling

--- src/main/java/com/mindtree/ira/controller/IRAService.java
+++ src/main/java/com/mindtree/ira/controller/IRAService.java
@@ -22,8 +22,9 @@ public class IRAService {
 		IRAServiceResponse serviceResponse = new IRAServiceResponse();
 		CustomerDAO customerDAO=new CustomerDAO();
 		ReservationDAO reservationDAO=new ReservationDAO();
-		if (responseBean.getResult().getAction()
-				.equalsIgnoreCase("input.welcome")) {
+		String inputAction = responseBean.getResult().getAction();
+		
+		if (inputAction.equalsIgnoreCase("input.welcome")) {
 			String customerName = customerDAO.getCustomerName(custId);
 			if (null == customerName) {
 				serviceResponse.setSpeech("Hi");
@@ -31,7 +32,7 @@ public class IRAService {
 				serviceResponse.setSpeech("Hi " + customerName);
 			}
 		}
-		else if(responseBean.getResult().getAction().equalsIgnoreCase("check_out.info")){
+		else if(inputAction.equalsIgnoreCase("check_out.info")){
 			ReservationInfo reservationInfo = getReservationInfo(custId,
 					reservationDAO);
 			SimpleDateFormat prounanceDateString = new SimpleDateFormat("dd MMM YYYY");
@@ -43,6 +44,21 @@ public class IRAService {
 					reservationDAO);
 			
 			
+		}
+		else if(inputAction.equalsIgnoreCase("pool.info")){
+			Context poolCrossSelling = new Context();
+			poolCrossSelling.setLifespan(1);
+			poolCrossSelling.setName("pool_cross_selling");
+			Context[] poolCrossSellingContextArray = {poolCrossSelling};
+			
+			serviceResponse.setContextOut(poolCrossSellingContextArray);
+			serviceResponse.setDisplayText("Our Swimming Pool will be open from 08:00 AM to 06:00 PM on all days. To use the pool, you need to have swim attrire. Id you don't have one, would you like me to get one for you? ");
+			serviceResponse.setSpeech("Our Swimming Pool will be open from 08:00 AM to 06:00 PM on all days. To use the pool, you need to have swim attrire. Id you don't have one, would you like me to get one for you? ");
+		}
+		else if(inputAction.equalsIgnoreCase("order.pool_attrire")){
+			
+			serviceResponse.setDisplayText("Got it. Your attrire will be delivered to your room.");
+			serviceResponse.setSpeech("Got it. Your attrire will be delivered to your room.");
 		}
 		else{
 			Context testContext = new Context();
---
+++ src/main/java/com/mindtree/ira/dao/PropertyDAO.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mindtree.ira.dao;
+
+import com.mindtree.ira.entity.PropertyAmenity;
+
+/**
+ * @author M1031960
+ *
+ */
+public class PropertyDAO {
+	
+	public PropertyAmenity getPropertyAmenityInfo(String propertyId, String amenityCode){
+		PropertyAmenity amenity = new PropertyAmenity();
+	}
+
+}
